e81bcf9cabb86ddd003fc35906c68edf7e0fc210
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2020-05-20 20:08:28 -0700

    V2
    Added Text Analysis namespace and resources

---
+++ TextAnalysis/TextValuePair.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace TimHanewichToolkit.TextAnalysis
+{
+    public class TextValuePair
+    {
+        public string Text { get; set; }
+        public float Value { get; set; }
+    }
+}
