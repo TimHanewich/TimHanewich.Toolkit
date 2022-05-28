a8adb047a16a105433b4952c59e64aa1baeb98b8
Tim Hanewich <tahanewich@live.com>
2022-05-28 07:15:12 -0400

    Default values for ODataSettings

--- src/OData/ODataSettings.cs
+++ src/OData/ODataSettings.cs
@@ -5,5 +5,10 @@ namespace TimHanewich.Toolkit.OData
     public class ODataSettings
     {
         public bool AllowMultiRowModification {get; set;}
+
+        public ODataSettings()
+        {
+            AllowMultiRowModification = false;
+        }
     }
 }
